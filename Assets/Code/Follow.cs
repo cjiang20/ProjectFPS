d30d1992acba160ae4514b712a1d8594c1c4389f
mathstar2 <matthewyi5951@gmail.com>
2022-03-10 16:05:37 -0500

    Added some basic camera controls

---
+++ Assets/Code/Follow.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public float speed = 200;
+    public float minRotation = -45;
+    public float maxRotation = 45;
+
+    public GameObject target;
+
+    Vector3 stepVector;
+
+    float XRotation;
+    float YRotation;
+    float ZRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        stepVector = speed * Vector3.forward;
+        target = GameObject.Find("Player Character");
+
+        XRotation = 0;
+        YRotation = 0;
+        ZRotation = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //This code follows the Me object, keeping the same position, but keeping the y axis parrallel to the ground (i.e. the global y axis)
+        //This allows the Me object to rotate on the "global axis."
+        transform.position = target.transform.position;
+
+        XRotation = 0;
+        YRotation = target.transform.eulerAngles.y;
+        ZRotation = 0;
+
+        Quaternion rots = Quaternion.Euler(XRotation, YRotation, ZRotation);
+
+        transform.rotation = rots;
+    }
+}
---
+++ Assets/Code/Turn.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour
+{
+    public float speed = 300;
+    public float minRotation = -45;
+    public float maxRotation = 45;
+
+    float XRotation;
+    float YRotation;
+    float ZRotation;
+
+
+    public GameObject target;
+
+    Vector3 stepVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        stepVector = speed * Vector3.forward;
+        target = GameObject.Find("Global Axis");
+
+        XRotation = 0;
+        YRotation = 0;
+        ZRotation = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Quits game when Escape is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        // Translates when using left click, forwards
+        if(Input.GetKey("w")) {
+            transform.Translate(Vector3.forward * 5 * Time.deltaTime, Space.Self);
+        }
+
+        // Translates when using right click, backwards
+        else if(Input.GetKey("s")) {
+            transform.Translate(Vector3.forward * -5 * Time.deltaTime, Space.Self);
+        }
+
+        // This code does the rotations proper, the target.transofrm.eulerAngles.y is the Global Axis's euler angles.
+        // This allows use to use the "Global Axis" in order to rotate properly.  
+        XRotation = transform.eulerAngles.x + (-1 * speed * Time.deltaTime * Input.GetAxis("Mouse Y"));
+        YRotation = target.transform.eulerAngles.y + (speed * Time.deltaTime * Input.GetAxis("Mouse X"));
+        ZRotation = 0;
+
+        // Lock rotation between 0-45, and 315-360
+        if(XRotation > 45f && XRotation <= 180f) {
+            XRotation = 45f;
+        }
+        if(XRotation > 180f && XRotation < 315f) {
+            XRotation = 315f;
+        }
+
+        // Quaternion values, as suggested by the assignment page.
+        Quaternion rots = Quaternion.Euler(XRotation, YRotation, ZRotation);
+
+        transform.rotation = rots;
+
+
+    }
+}
