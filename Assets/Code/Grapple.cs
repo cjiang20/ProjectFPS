e0cae7ad7ca90fd239096df703a4984d493ced6c
Christopher Jiang <cjiang@Christophers-MacBook-Air.local>
2022-03-15 15:59:28 -0400

    fixed compiler error

--- Assets/Code/Grapple.cs
+++ Assets/Code/Grapple.cs
@@ -8,6 +8,7 @@ public class Grapple : MonoBehaviour
     private Rigidbody rb;
     private float length;
     private Vector3 tetherPoint;
+    public Camera fpsCam;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,17 +19,14 @@ public class Grapple : MonoBehaviour
     void Update()
     {
         if(Input.GetKey("q")) {
-            Grapple();
+            GrappleHook();
         }
     }
 
-    void Grapple() {
+    void GrappleHook() {
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit)) {
             Target target = hit.transform.GetComponent<Target>();
-            if (target != null) {
-                target.TakeDamage(damage);
-            }
         }
     }
 }
