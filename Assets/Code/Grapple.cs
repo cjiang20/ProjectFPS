5068d33b0ce2ba6189f3b9dd0bff946c6c8a700d
mathstar2 <matthewyi5951@gmail.com>
2022-03-15 01:32:39 -0400

    Update Grapple.cs

--- Assets/Code/Grapple.cs
+++ Assets/Code/Grapple.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Grapple : MonoBehaviour
 {
+    private bool attached = false;
+    private Rigidbody rb;
+    private float length;
+    private Vector3 tetherPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey("q")) {
-            transform.Translate(Vector3.forward * 5 * Time.deltaTime, Space.Self);
+            Grapple();
+        }
+    }
+
+    void Grapple() {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit)) {
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null) {
+                target.TakeDamage(damage);
+            }
         }
     }
 }
