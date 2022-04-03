750c5b8f352175e53a81cc727772fe19be435b32
mathstar2 <matthewyi5951@gmail.com>
2022-04-02 23:49:11 -0400

    Grapple Hook hit detection completed
    The grappling function is still under construction, but it does detect when it "Grapples" to an object.

--- Assets/Code/Grapple.cs
+++ Assets/Code/Grapple.cs
@@ -8,25 +8,58 @@ public class Grapple : MonoBehaviour
     private Rigidbody rb;
     private float length;
     private Vector3 tetherPoint;
-    public Camera fpsCam;
+    private Camera viewPoint;
+
     // Start is called before the first frame update
     void Start()
     {
+        viewPoint = Camera.main;
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("q")) {
-            GrappleHook();
+        if(Input.GetKeyDown("q")) {
+            if(!attached) {
+                GrappleHook();
+            }
+            else {
+                EndGrapple();
+            }
         }
+
+        if(attached) {
+            grapplePhysics();
+        }
+    }
+
+    void grapplePhysics() {
+        // Vector3 directionToGrapple = Vector3.Normalize(tetherPoint - transform.position);
+        // float currentDistanceToGrapple = Vector3.Distance(tetherPoint, transform.position);
+
+        // float speedTowardsGrapplePoint = Mathf.Round(Vector3.Dot(rb.velocity, directionToGrapple) * 100) / 100;
+
+        // if (speedTowardsGrapplePoint < 0) {
+        //     if (currentDistanceToGrapple > length) {
+        //         rb.velocity -= speedTowardsGrapplePoint * directionToGrapple;
+        //         rb.position = tetherPoint - directionToGrapple * length;
+        //     }
+        // }
     }
 
     void GrappleHook() {
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit)) {
-            Target target = hit.transform.GetComponent<Target>();
+        if (Physics.Raycast(viewPoint.transform.position, viewPoint.transform.forward, out hit)) {
+            tetherPoint = hit.point;
+            attached = true;
+            length = Vector3.Distance(tetherPoint, transform.position);
+            print("WE GOTTEM");
+            print(tetherPoint);
         }
+    }
+    
+    void EndGrapple() {
+        attached = false;
     }
 }
