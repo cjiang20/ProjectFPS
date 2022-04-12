5f0b158cd229e01b79cad2646f9352882ef55ca4
kirvanfh <kirvanfh@gmail.com>
2022-04-12 17:16:24 -0400

    Clock

---
+++ Assets/Code/Clock.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Clock : MonoBehaviour
+{  
+    public Text textClock;
+    private float time;
+    private DateTime oldTime;
+    //Testing
+    void Start (){ 
+        time = 0;
+   }
+    void Update (){
+        time = time + Time.deltaTime;
+        TimeSpan currTime = TimeSpan.FromSeconds(time);
+        textClock.text = currTime.ToString(@"ss\:ff");
+   }
+}
+
